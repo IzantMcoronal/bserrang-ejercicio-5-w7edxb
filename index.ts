f2bf4d87a3c5794772e2b51022409ad17fada839
IzantMcoronal <izant2020@gmail.com>
2020-11-12 09:24:12 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,15 +1,21 @@
 // Tipad
 // Para determinar el tipo de los parámetros de la función, debes dar por hecho que los tipos van a ser siempre los de las dos variables que se añaden en la llamada de la linea 20.
 
+// declaramos la variable a como tipo numero
 let a : number = 5;
+
+//Declaramos la variable b como String
 let b : string = "texto";
+
+// Aqui usamos el operador "|" ya que la variable puede ser de dos tipos, tanto un numero como booleana
 let c : boolean | number = true;
 
 if (Math.random() >= 0.5) {
   c = 100;
 }
 
-function d(param1: number, param2: boolean | number) {
+// Typamos el parametro de la funcion 1 al ser el operador a como numero y el parametro 2 como booleana y numero ya que es el operador c. La funcion la tipamos de 2 tipos, ya que puede dar 2 resultados, o bien una suma numerica o bien un string. 
+function d(param1: number, param2: boolean | number):number | String {
   if (typeof param2 === "number") {
     return param1 + param2;
   } else {
