fde097abae8961e6ec26ec4c25a303e6985bf750
IzantMcoronal <izant2020@gmail.com>
2020-11-12 09:10:22 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,19 +1,15 @@
 // Tipad
 // Para determinar el tipo de los parámetros de la función, debes dar por hecho que los tipos van a ser siempre los de las dos variables que se añaden en la llamada de la linea 20.
 
-let a;
-let b;
-let c;
-
-a = 5;
-b = "texto";
-c = true;
+let a : number = 5;
+let b : string = "texto";
+let c : boolean | number = true;
 
 if (Math.random() >= 0.5) {
   c = 100;
 }
 
-function d(param1, param2) {
+function d(param1: number, param2: boolean | number) {
   if (typeof param2 === "number") {
     return param1 + param2;
   } else {
